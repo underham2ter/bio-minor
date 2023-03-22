7d0993bce5fd818751fb982a69bf78e4920d5745
underham2ter <56203128+underham2ter@users.noreply.github.com>
2023-03-22 21:14:29 +0300

    Add files via upload

--- HW5/HW2-Y1-S2.py
+++ HW5/HW2-Y1-S2.py
@@ -9,24 +9,11 @@ TP53 = "ENSG00000141510"
 SMAD4 = "ENSG00000141646"
 CDKN2A = "ENSG00000147889"
 ARID1A = "ENSG00000117713"
+APC = "ENSG00000134982"
+PIK3CA = "ENSG00000121879"
 
 patients = list(set(df_o.icgc_donor_id.values))
 # print(patients)
-
-# for p in patients:
-#
-#     df = df_o[df_o.icgc_donor_id == p]
-#     n_kras = len(df[df.gene_affected == KRAS])
-#     n_tp53 = len(df[df.gene_affected == TP53])
-#     if n_tp53 > 0 and n_kras > 0:
-#         print(p)
-#         print("Mutations in KRAS gene: %s" % n_kras)
-#         print("Mutations in TP53 gene: %s" % n_tp53)
-#         print("Mutations in SMAD4 gene: %s" % len(df[df.gene_affected == SMAD4]))
-#         print("Mutations in CDKN2A gene: %s" % len(df[df.gene_affected == CDKN2A]))
-#         print("Mutations in ARID1A gene: %s" % len(df[df.gene_affected == ARID1A]))
-#     else:
-#         continue
 
 df = df_o[df_o.icgc_donor_id == id]
 n_kras = len(df[df.gene_affected == KRAS])
@@ -35,5 +22,7 @@ n_tp53 = len(df[df.gene_affected == TP53])
 print("Mutations in KRAS gene: %s" % n_kras)
 print("Mutations in TP53 gene: %s" % n_tp53)
 print("Mutations in SMAD4 gene: %s" % len(df[df.gene_affected == SMAD4]))
-print("Mutations in CDKN2A gene: %s" % len(df[df.gene_affected == CDKN2A]))
-print("Mutations in ARID1A gene: %s" % len(df[df.gene_affected == ARID1A]))
+# print("Mutations in CDKN2A gene: %s" % len(df[df.gene_affected == CDKN2A]))
+# print("Mutations in ARID1A gene: %s" % len(df[df.gene_affected == ARID1A]))
+print("Mutations in APC gene: %s" % len(df[df.gene_affected == APC]))
+print("Mutations in PIK3CA gene: %s" % len(df[df.gene_affected == PIK3CA]))
